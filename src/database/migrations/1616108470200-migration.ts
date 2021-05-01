32450cea50c6e8a290199d2f7c1b822d3b97b6d3
Messias <messiasolimpio@hotmail.com>
2021-04-30 21:03:35 -0300

    Add table genre and order

--- src/database/migrations/1616108470200-migration.ts
+++ src/database/migrations/1616108470200-migration.ts
@@ -11,7 +11,13 @@ export class migration1616108470200 implements MigrationInterface {
       'CREATE TABLE "games" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "title" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_c9b16b62917b5595af982d66337" PRIMARY KEY ("id"))',
     );
     await queryRunner.query(
-      'CREATE TABLE "users_games_games" ("usersId" uuid NOT NULL, "gamesId" uuid NOT NULL, CONSTRAINT "PK_cd4067d574477fd5c7693bc7872" PRIMARY KEY ("usersId", "gamesId"))',
+      'CREATE TABLE "genres" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "title" character varying NOT NULL, CONSTRAINT "PK_4bb32fe50fbd1f240c2722eecdbb4684" PRIMARY KEY ("id"))',
+    );
+    await queryRunner.query(
+      'CREATE TABLE "orders" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_12c500ed0b7879105fb46af0f246be87" PRIMARY KEY ("id"))',
+    );
+    await queryRunner.query(
+      'CREATE TABLE "users_games_games" ("usersId" uuid NOT NULL, "gamesId" uuid NOT NULL, "genreId" uuid NOT NULL, "orderId" uuid NOT NULL, CONSTRAINT "PK_cd4067d574477fd5c7693bc7872" PRIMARY KEY ("usersId", "gamesId", "genreId", "orderId"))',
     );
     await queryRunner.query(
       'CREATE INDEX "IDX_e5263d029d8644de829aae5c35" ON "users_games_games" ("usersId") ',
@@ -20,10 +26,22 @@ export class migration1616108470200 implements MigrationInterface {
       'CREATE INDEX "IDX_934b0d8f9d0084c97d3876ad32" ON "users_games_games" ("gamesId") ',
     );
     await queryRunner.query(
+      'CREATE INDEX "IDX_080a38f3c1d8b442cb182a6560ed355a" ON "users_games_games" ("genreId") ',
+    );
+    await queryRunner.query(
+      'CREATE INDEX "IDX_69dfcb072307d1f23d6dae1ab92fffbf" ON "users_games_games" ("orderId") ',
+    );
+    await queryRunner.query(
       'ALTER TABLE "users_games_games" ADD CONSTRAINT "FK_e5263d029d8644de829aae5c35a" FOREIGN KEY ("usersId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION',
     );
     await queryRunner.query(
       'ALTER TABLE "users_games_games" ADD CONSTRAINT "FK_934b0d8f9d0084c97d3876ad32d" FOREIGN KEY ("gamesId") REFERENCES "games"("id") ON DELETE CASCADE ON UPDATE NO ACTION',
+    );
+    await queryRunner.query(
+      'ALTER TABLE "users_games_games" ADD CONSTRAINT "FK_e2866c8f54f85c4cdff068015b247551" FOREIGN KEY ("genreId") REFERENCES "genres"("id") ON DELETE CASCADE ON UPDATE NO ACTION',
+    );
+    await queryRunner.query(
+      'ALTER TABLE "users_games_games" ADD CONSTRAINT "FK_4fee2f89b2b0917db0f2d80af8d4b79a" FOREIGN KEY ("orderId") REFERENCES "orders"("id") ON DELETE CASCADE ON UPDATE NO ACTION',
     );
   }
 
